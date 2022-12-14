b8bba4eacd05d2e5e827e687a1c073aef1f0d14c
Marino <marinogomez24@gmail.com>
2022-12-13 20:08:56 -0400

    first basic scene

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Basic from "./pages/Basic/index.jsx";
+import BasicScene from "./pages/Basic/01_basic_scene/index.jsx";
 import Home from "./pages/index.jsx";
 
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="basic" element={<Basic />} />
+        <Route path="basic/01_basic_scene" element={<BasicScene />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Basic/index.jsx
+++ src/pages/Basic/index.jsx
@@ -1,5 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Basic = () => {
-  return <h1>Basic Modules</h1>;
+  return (
+    <div>
+      <h1>Basic Modules</h1>
+      <Link
+        to={{
+          pathname: "/basic/01_basic_scene",
+        }}
+      >
+        <button>basic scene</button>
+      </Link>
+    </div>
+  );
 };
 
 export default Basic;
