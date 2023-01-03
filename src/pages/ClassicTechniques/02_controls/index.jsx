153e4425f70bf4ba384f592ead4d216d5de4b306
Marino <marinogomez24@gmail.com>
2023-01-02 20:04:45 -0400

    Fixes: - Transform options weren't showing up properly.

--- src/pages/ClassicTechniques/02_controls/index.jsx
+++ src/pages/ClassicTechniques/02_controls/index.jsx
@@ -1,34 +1,34 @@
-import { useRef, useEffect, useState } from "react";
+import { useRef } from "react";
 import { Canvas } from "@react-three/fiber";
 import {
   Html,
   TransformControls,
   OrbitControls,
   Text,
-  Float,
   MeshReflectorMaterial,
+  Float,
 } from "@react-three/drei";
 
 import "../../home.css";
-
-function Plane(props) {
-  return (
-    <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10} {...props}>
-      <planeGeometry />
-      <MeshReflectorMaterial
-        mirror={0.5}
-        blur={[1000, 1000]}
-        mixBlur={1}
-        color="greenyellow"
-      />
-    </mesh>
-  );
-}
 
 function ControlsScene() {
   const getInnerHeight = () => window.innerHeight - 57;
   const cube = useRef();
   const sphere = useRef();
+
+  function Plane() {
+    return (
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <MeshReflectorMaterial
+          mirror={0.5}
+          blur={[1000, 1000]}
+          mixBlur={1}
+          color="greenyellow"
+        />
+      </mesh>
+    );
+  }
 
   const Label = ({ text }) => {
     return (
@@ -46,37 +46,38 @@ function ControlsScene() {
 
   function CubeMesh() {
     return (
-      <mesh ref={cube} castShadow position-x={2} scale={1.5}>
-        <boxGeometry />
-        <meshStandardMaterial color="mediumpurple" />
-      </mesh>
+      <group renderOrder={0}>
+        <TransformControls object={cube} mode="rotate">
+          <mesh ref={cube} castShadow position-x={2} scale={1.5}>
+            <boxGeometry />
+            <meshStandardMaterial color="mediumpurple" />
+          </mesh>
+        </TransformControls>
+      </group>
     );
   }
 
   function SphereMesh() {
     return (
-      <mesh ref={sphere} castShadow position-x={-2}>
-        <sphereGeometry />
-        <meshStandardMaterial color="orange" />
-        <Label text={"This is a sphere👍"} />
-      </mesh>
+      <group renderOrder={0}>
+        <TransformControls object={sphere} mode="translate">
+          <mesh ref={sphere} castShadow position-x={-2}>
+            <sphereGeometry />
+            <meshStandardMaterial color="orange" />
+            <Label text={"This is a sphere👍"} />
+          </mesh>
+        </TransformControls>
+      </group>
     );
   }
   return (
     <div style={{ height: getInnerHeight(), backgroundColor: "whitesmoke" }}>
-      <Canvas dpr={[1, 2]} shadows camera={{ position: [-5, 5, 5], fov: 50 }}>
-        <ambientLight />
-        <spotLight
-          angle={0.25}
-          penumbra={0.5}
-          position={[10, 10, 5]}
-          castShadow
-        />
-        <Plane />
-        <TransformControls object={cube} mode="rotate" />
-        <TransformControls object={sphere} mode="translate" />
-        <CubeMesh />
-        <SphereMesh />
+      <Canvas
+        dpr={[1, 2]}
+        shadows
+        camera={{ position: [-5, 5, 5], fov: 50 }}
+        legacy={true}
+      >
         <Float speed={5}>
           <Text
             color="salmon"
@@ -89,7 +90,19 @@ function ControlsScene() {
             <meshNormalMaterial />
           </Text>
         </Float>
-        <OrbitControls makeDefault />
+        <ambientLight />
+        <spotLight
+          angle={0.25}
+          penumbra={0.5}
+          position={[10, 10, 5]}
+          castShadow
+        />
+        <Plane />
+        <group>
+          <CubeMesh />
+          <SphereMesh />
+          <OrbitControls makeDefault />
+        </group>
       </Canvas>
     </div>
   );
